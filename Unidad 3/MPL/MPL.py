fca7ea218ff2f1ffad9bc564c5c0f52e9c09e0fa
ZETAOCHO <jose.angel.corral.bujanda@gmail.com>
2025-06-23 15:23:08 +0000

    Installacion de matplotlib

--- Unidad 3/MPL/MPL.py
+++ Unidad 3/MPL/MPL.py
@@ -8,12 +8,19 @@
 #Mapas de color
 #Metodologia general para construir graficas con MatPlotLib:
 #1.-Se importa el modulo pyplot
+#   pip install matplotlib
 #2.-Se define la figura que contendra la grafica, que se refiere a la region donde se 
 #   dibujara y los ejes que graficaran los datos (Ventana).Para esto se emplea la función
 #   subplots().
 #3.-Se dibujan los datos sobre los ejes. En este paso se emplean diversas funciones 
 #   dependiendo de la grafica que se requiera.
-#4.-Personalizacion de la grafica 
-
+#4.-Personalizacion de la grafica
+#   Titulo
+#   Leyenda 
+#   Colores 
+#   Rejilla 
+#   etc
+#5.-Guardar la grafica, se utiliza la funcion savefig()
+#6.-Despliegue de la grafica, se utiliza lafuncion show()
 
 
