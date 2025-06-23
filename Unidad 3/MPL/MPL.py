fb5ceec870f1227471e76d409296fad6e3fc2147
ZETAOCHO <jose.angel.corral.bujanda@gmail.com>
2025-06-23 15:36:36 +0000

    Se agrego el tipode graficas disponibles

--- Unidad 3/MPL/MPL.py
+++ Unidad 3/MPL/MPL.py
@@ -14,6 +14,15 @@
 #   subplots().
 #3.-Se dibujan los datos sobre los ejes. En este paso se emplean diversas funciones 
 #   dependiendo de la grafica que se requiera.
+#   Histograma
+#   barras
+#   de sectores
+#   de caja y bigotes
+#   lineas
+#   areas
+#   Color
+#   contorno 
+#   de violines
 #4.-Personalizacion de la grafica
 #   Titulo
 #   Leyenda 
@@ -24,3 +33,13 @@
 #6.-Despliegue de la grafica, se utiliza lafuncion show()
 
 
+#El primer paso es importar el modulo pyplot con el alias plt
+import matplotlib.pyplot as plt
+#Despues Creamos la figura y los ejes
+fig, ax = plt.subplots()
+#En este caso se va a dibujar una serie de puntos
+ax.scatter(x=[1, 2, 3], y=[3, 2, 1])
+#Se procede aGuardar el grafico en formato PNG
+plt.savefig("Diagrama de dispersión.png")
+#Por ultimo se muestra el grafico
+plt.show()
