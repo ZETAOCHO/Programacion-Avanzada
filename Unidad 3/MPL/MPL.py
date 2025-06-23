c2763464af3d1c5657595ece37435935f3ec95a2
ZETAOCHO <jose.angel.corral.bujanda@gmail.com>
2025-06-23 08:39:02 -0600

    MPL

---
+++ Unidad 3/MPL/MPL.py
@@ -0,0 +1,14 @@
+#Permite crear y personalizar diversos tipos de graficas,como las que se mencionan
+#a continuacion:
+#Histogamas
+#Diagramas de areas
+#Diagramas de linea
+#Graficas de barra
+#Graaficas de contorno
+#Mapas de color
+#Metodologia general para construir graficas con MatPlotLib:
+#1.-Se importa el modulo pyplot
+#2.-Se define la figura que contendra la grafica, que se refiere a la region donde se 
+#   dibujara y los ejes que graficaran los datos () 
+
+
