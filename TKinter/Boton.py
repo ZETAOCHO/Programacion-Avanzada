57cf2d36665479caae615ed64056a055a78a6244
ZETAOCHO <jose.angel.corral.bujanda@gmail.com>
2025-06-30 08:36:08 -0600

    tk.buttom

---
+++ TKinter/Boton.py
@@ -0,0 +1,10 @@
+#Boton en TKinter 
+#Los botones son los controles mas empleados en aplicaciones graficas 
+#Un boton es un elemento con un texto y/o imagen  que al ser activados pr el usuario
+#(presionado), ejecuta una operacion definida. En TKinter, esta representado por las
+#clases tk.Button y ttk.Button; como cualquier otro control de TKinter, para crear un boton
+#debemos crear una instancia de la clase correspondiente.
+#En el siguiente codigo e crea una ventanacon un boton, cuyo texto esta representado por 
+#el parametro text(IMEC91M)
+
+import TKinter as tk
